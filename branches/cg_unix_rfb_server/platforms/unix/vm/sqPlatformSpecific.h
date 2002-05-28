f42873f9dcee611cd75fec95f8afdef5fdc1bd0d
nobody <nobody@fa1542d4-bde8-0310-ad64-8ed1123d492a>
2002-05-28 20:16:54 +0000

    This commit was manufactured by cvs2svn to create branch 'cg_unix_rfb_server'.

---
+++ branches/cg_unix_rfb_server/platforms/unix/vm/sqPlatformSpecific.h
@@ -0,0 +1,81 @@
+/* Unix sqPlatformSpecific.h -- Platform-specific prototypes and definitions */
+
+/* How to use this file:
+   This file is for general platform-specific macros and declarations.
+   The goal is to keep most of the other header files generic across platforms.
+   To override a definition or macro from sq.h, you must first #undef it, then
+   provide the new definition.
+
+*/
+
+
+#ifndef SQ_PLATFORM_SPECIFIC_H
+#define SQ_PLATFORM_SPECIFIC_H
+
+#ifdef UNIX
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+/* off_t should be available (XXX though really, we should
+   autoconf-check this, and substitute whatever fseeko is using) */
+typedef off_t squeakFileOffsetType;
+
+#undef sqFTruncate
+#undef fseek
+#undef ftell
+
+
+#define sqFTruncate(fp, offs) ftruncate(fileno(fp),offs)
+#define fseek(fp, offs,type) fseeko(fp, offs,type)
+#define ftell(fp) ftello(fp)
+
+/* unix-specific prototypes and definitions */
+void aioPollForIO(int microSeconds, int extraFd);  /* XXX should no longer be needed -lex */
+#define SQ_FORM_FILENAME        "squeak-form.ppm"
+
+/* undefine clock macros that are implemented as functions */
+#undef ioMSecs
+#undef ioMicroMSecs
+#undef ioLowResMSecs
+
+
+
+
+/* use non-default heap-allocation functions; see sqUnixMemory.c */
+#undef sqAllocateMemory
+#undef sqGrowMemoryBy
+#undef sqShrinkMemoryBy
+#undef sqMemoryExtraBytesLeft
+
+void * sqAllocateMemory(int minHeapSize, int desiredHeapSize);
+int sqGrowMemoryBy(int oldLimit, int delta);
+int sqShrinkMemoryBy(int oldLimit, int delta);
+int sqMemoryExtraBytesLeft(int includingSwap);
+  
+
+
+
+
+#ifdef sqImageFileOpen  /* this is horrible, but is necessary because
+                           plugins don't include sq.h; so, we should
+                           redefine these macros only when all of sq.h
+                           is being used */
+
+/* use non-default image IO functions; see sqUnixImage.c */
+#undef sqImageFileOpen
+#undef sqImageFileStartLocation
+
+sqImageFile sqImageFileOpen(const char *fileName, const char *mode);
+int sqImageFileStartLocation(sqImageFile file, const char *fileName, int size);
+#endif
+
+
+#else
+
+#error This sqPlatformSpecific.h file is for Unix; you either have the wrong source code, or you forgot to -DUNIX
+
+#endif /* UNIX */
+
+#endif  /* SQ_PLATFORM_SPECIFIC_H */
