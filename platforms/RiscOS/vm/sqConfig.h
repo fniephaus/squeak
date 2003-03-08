ceee33f6e0836065d3b3c712148963ddd162a40a
nobody <nobody@fa1542d4-bde8-0310-ad64-8ed1123d492a>
2003-03-08 21:26:57 +0000

    This commit was manufactured by cvs2svn to create tag 'win32-3-4-1'.

--- platforms/RiscOS/vm/sqConfig.h
+++
@@ -1,9 +0,0 @@
-/* Acorn sqConfig.h -- platform identification and configuration */
-
-
-#if defined(ACORN)
-# if defined(SQ_CONFIG_DONE)
-#   error configuration conflict
-# endif
-# define SQ_CONFIG_DONE
-#endif
