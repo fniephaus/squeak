f4cdcc83d0007e1bdfc76588e06c8fd5a255a43d
piumarta <piumarta@fa1542d4-bde8-0310-ad64-8ed1123d492a>
2005-03-19 23:52:48 +0000

    3.7-7 unix release

---
+++ branches/unix-3.7-7/platforms/Mac OS/vm/sqMacImageIO.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+*   PROJECT: Squeak Headers
+*   FILE:    sqMacImageIO.c
+*   CONTENT: 
+*
+*   AUTHOR:  John Maloney, John McIntosh, and others.
+*   ADDRESS: 
+*   EMAIL:   johnmci@smalltalkconsulting.com
+*   RCSID:   $Id: sqMacImageIO.h,v 1.2 2003/12/02 04:52:35 johnmci Exp $
+*
+*   NOTES: 
+*  Feb 22nd, 2002, JMM moved code into 10 other files, see sqMacMain.c for comments
+ 3.7.0bx Nov 24th, 2003 JMM gCurrentVMEncoding
+****************************************************************************/
+
+#if TARGET_API_MAC_CARBON
+    #include <Carbon/Carbon.h>
+#endif
+
