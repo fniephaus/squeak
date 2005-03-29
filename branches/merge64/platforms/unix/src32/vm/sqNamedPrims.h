3cbae658cfeb494712cec04636e2aef1a95e77bb
piumarta <piumarta@fa1542d4-bde8-0310-ad64-8ed1123d492a>
2005-03-29 11:06:01 +0000

    initial import of 32- and 64-bit generated sources

---
+++ branches/merge64/platforms/unix/src32/vm/sqNamedPrims.h
@@ -0,0 +1,16 @@
+/* This is an automatically generated table of all builtin modules in the VM */
+
+extern sqExport vm_exports[];
+extern sqExport os_exports[];
+extern sqExport B2DPlugin_exports[];
+extern sqExport BitBltPlugin_exports[];
+extern sqExport FilePlugin_exports[];
+
+sqExport *pluginExports[] = {
+	vm_exports,
+	os_exports,
+	B2DPlugin_exports,
+	BitBltPlugin_exports,
+	FilePlugin_exports,
+	NULL
+};
---
+++ branches/merge64/platforms/unix/src64/vm/interp.h
@@ -0,0 +1,3 @@
+/* Automatically generated from Squeak on #(27 March 2005 6:13:27 pm) */
+
+#define SQ_VI_BYTES_PER_WORD 8
