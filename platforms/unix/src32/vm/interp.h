9c2689a55b57c29c1ddab2fb5ebe08701a455176
piumarta <piumarta@fa1542d4-bde8-0310-ad64-8ed1123d492a>
2005-03-31 23:15:22 +0000

    new src

--- platforms/unix/src32/vm/interp.h
+++
@@ -1,3 +0,0 @@
-/* Automatically generated from Squeak on #(27 March 2005 1:43:50 pm) */
-
-#define SQ_VI_BYTES_PER_WORD 4
--- platforms/unix/src64/vm/interp.h
+++
@@ -1,3 +0,0 @@
-/* Automatically generated from Squeak on #(27 March 2005 6:13:27 pm) */
-
-#define SQ_VI_BYTES_PER_WORD 8
